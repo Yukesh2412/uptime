5e8a4d05510134167eccf3a38ab1461848fe2a0c
Yukesh.HARIKRISHNAN <Yukesh.HARIKRISHNAN@interproglobal.com>
2024-06-29 10:46:56 +0530

    preference manager inprogress

---
+++ app/src/main/java/com/yukesh/uptime/utils/PreferenceManager.kt
@@ -0,0 +1,19 @@
+//package com.yukesh.uptime.utils
+//
+//import android.content.Context
+//import android.content.SharedPreferences
+//
+//class PreferenceManager(context: Context) {
+//    private val sharedPreferences: SharedPreferences =
+//        context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+//
+//    fun saveData(key:String, value:Boolean){
+//        val editor= sharedPreferences.edit()
+//        editor.putBoolean(key,value)
+//        editor.apply()
+//    }
+//
+//    fun getData(key: String, defaultValue: Boolean): Boolean {
+//        return sharedPreferences.getBoolean(key, defaultValue) ?: defaultValue
+//    }
+//}
